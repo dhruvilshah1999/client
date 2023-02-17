26297e7942abb96801923005692455d7a65df902
lizaviradiya1408 <107081867+lizaviradiya1408@users.noreply.github.com>
2023-02-17 09:48:02 -0500

    Working on Dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -2,32 +2,6 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import DasboardLayout from '../NewComponents/DashboardLayout'
 
-const Dashboard = () => {
-    // login user data
-    const getAdminData = async () => {
-        try {
-            const res = await axios.post(
-                "/api/v1/user/getAdminData", 
-                {},
-                {
-                    headers: {
-                        Authorization: "Bearer " + localStorage.getItem("token"),
-                    },
-                }
-            );
-        } catch (error) {
-            console.log(error);
-        }
-    };
 
-    useEffect(() => {
-        getAdminData();
-    }, []);
-    return (
-        <DasboardLayout>
-            <h1>Dashboard</h1>
-        </DasboardLayout>
-    );
-};
 
 export default Dashboard;
