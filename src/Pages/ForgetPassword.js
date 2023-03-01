99853a584277c0efec260a96d4e95718a6e85a8f
nizamdiwan <52311458+nizamdiwan@users.noreply.github.com>
2023-03-01 08:45:12 -0500

    working on forgot password page

--- src/Pages/ForgetPassword.js
+++ src/Pages/ForgetPassword.js
@@ -1,15 +1,4 @@
 const ForgetPassword = () => {
-    const resetPassword = async (values)=> {
-
-    }
-    
-    return(
-        <>
-            <div>
-                Forgot Password Page
-            </div>
-        </>
-    )
-}
+   
 
 export default ForgetPassword
