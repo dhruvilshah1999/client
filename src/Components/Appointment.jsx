c7359d59367186531eec3097659de5f67611b358
lizaviradiya1408 <107081867+lizaviradiya1408@users.noreply.github.com>
2023-03-09 20:20:34 -0500

    working on appointment nav

--- src/Components/Appointment.jsx
+++ src/Components/Appointment.jsx
@@ -4,22 +4,7 @@ import { UpdatesData } from "../TempData/Data";
 
 const Appointment = () => {
   return (
-    <div className="Updates">
-      {UpdatesData.map((update) => {
-        return (
-          <div className="update">
-            <img src={update.img} alt="profile" />
-            <div className="noti">
-              <div  style={{marginBottom: '0.5rem'}}>
-                <span>{update.name}</span>
-                <span> {update.noti}</span>
-              </div>
-                <span>{update.time}</span>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    
   );
 };
 
